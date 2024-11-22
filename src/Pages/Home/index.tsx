c40e55a816c770ab0a0b1087b30430472a9156e2
oluseunakintayo <oluseun.akintayo@gmail.com>
2024-11-22 11:39:39 +0100

    Updated projects: InvoiceMaker

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -103,13 +103,10 @@ const Home = () => {
         </div>
       </section>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t bg-purple-600">
-        <p className="text-xs text-white">© {new Date().getFullYear()} InvoiceMaker. All rights reserved.</p>
+        <p className="text-xs text-white">© {new Date().getFullYear()} <a href="https://techydna.vercel.app/" target='_blank'>TechyDNA</a>. All rights reserved.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link className="text-xs hover:underline underline-offset-4 text-white" to="#">
             Terms of Service
-          </Link>
-          <Link className="text-xs hover:underline underline-offset-4 text-white" to="#">
-            Privacy
           </Link>
         </nav>
       </footer>
